5c80405015f7aa2d7869b7697ef4f854ba1a4d30
Student 2 <user2@tc.geeksforless.net>
2017-08-31 16:21:59 +0300

    rename controllers

---
+++ application/controllers/ControllerAuthorization.pm
@@ -0,0 +1,17 @@
+package controllers::AuthorizationController;
+
+use strict;
+use warnings;
+
+use vars qw(@ISA);
+use core::Controller;
+@ISA = qw(core::Controller);
+
+
+
+sub actionIndex
+{
+print 'AuthorizationController';
+}
+
+1;
---
+++ application/controllers/ControllerRegistration.pm
@@ -0,0 +1,17 @@
+package controllers::RegistrationController;
+
+use strict;
+use warnings;
+
+use vars qw(@ISA);
+use core::Controller;
+@ISA = qw(core::Controller);
+
+
+
+sub actionIndex
+{
+print 'RegistrationController';
+}
+
+1;
