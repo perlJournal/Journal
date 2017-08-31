1f1d462b9597d4483f5f315d8e8a598fc22db92e
perlJournal <31458438+perlJournal@users.noreply.github.com>
2017-08-31 17:24:25 +0300

    Create ProfileEditController.pm

---
+++ application/controllers/ProfileEditController.pm
@@ -0,0 +1,17 @@
+package controllers::ProfileEditController;
+
+use strict;
+use warnings;
+
+use vars qw(@ISA);
+use core::Controller;
+@ISA = qw(core::Controller);
+
+
+
+sub actionIndex
+{
+print 'ProfileEditController';
+}
+
+1;
