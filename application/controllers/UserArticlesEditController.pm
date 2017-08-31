38b16e8eb41067db451e6932e379cc1cba9ffa4a
perlJournal <31458438+perlJournal@users.noreply.github.com>
2017-08-31 17:27:08 +0300

    Create UserArticlesEditController.pm

---
+++ application/controllers/UserArticlesEditController.pm
@@ -0,0 +1,17 @@
+package controllers::UserArticlesEditController;
+
+use strict;
+use warnings;
+
+use vars qw(@ISA);
+use core::Controller;
+@ISA = qw(core::Controller);
+
+
+
+sub actionIndex
+{
+print 'UserArticlesEditController';
+}
+
+1;
