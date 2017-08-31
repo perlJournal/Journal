a9ab7f8e1640dfd96dc8d84a2460f6a1d6719bbc
Student 2 <user2@tc.geeksforless.net>
2017-08-31 19:42:13 +0300

    User Edit Model

--- application/models/Users.pm
+++ application/models/Users.pm
@@ -6,9 +6,28 @@ use warnings;
 
 use vars qw(@ISA);
 use core::Model;
+use utils::WorkDb;
 @ISA = qw(core::Model);
+use DBI;
+use Data::Dumper;
 
-sub msg
+
+sub actionEdit
 {
-	print 'Users';
+	my($self,$id)= @_;
+	my $obj = utils::WorkDb->new();
+	$obj->connectToDb();
+	my $data = $obj->upDelIns("UPDATE Data SET `key` = 'test' , `value` = 'cora' WHERE `key`= '$id' ");	
+
 }
+
+sub getUserData
+{
+	my($self,$login)= @_;
+	my $obj = utils::WorkDb->new();
+	$obj->connectToDb();
+	my $dataUser = $obj->exeSelect("Select * from Data where `key`='$login' ");
+	return $dataUser;
+}
+
+1;
