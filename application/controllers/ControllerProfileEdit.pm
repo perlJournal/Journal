b74319c12556f598ffc1115f18211a468dd6640d
Student 2 <user2@tc.geeksforless.net>
2017-09-01 14:40:33 +0300

    View update user

--- application/controllers/ControllerProfileEdit.pm
+++ application/controllers/ControllerProfileEdit.pm
@@ -11,7 +11,14 @@ use core::Controller;
 
 sub actionIndex
 {
-print 'ProfileEditController';
+	my $model = models::Users->new();
+    my $getDataUserById = $model->getUserData('test');
+    my $editUser =  $model->actionEdit('test');
+	my $view = views::ViewProfileEdit->new();
+	my $data = $view->getTemplate('profile'); 
+    my $obj =  $view->generateTemplate($data,$editUser);
+	$view->viewTemplate($obj);
+
 }
 
 1;
