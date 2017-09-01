7b914aee0f1f1072226ef7359364f2f7cac1444b
Your Name <you@example.com>
2017-09-01 13:42:00 +0300

    mainController plus mainView

--- application/controllers/ControllerUserArticles.pm
+++ application/controllers/ControllerUserArticles.pm
@@ -5,13 +5,20 @@ use warnings;
 
 use vars qw(@ISA);
 use core::Controller;
+use models::Articles;
+use views::ViewMain;
+use Data::Dumper;
+
 @ISA = qw(core::Controller);
 
+my $modelObj = model::Articles->new();
 
 
 sub actionIndex
 {
-print 'UserArticlesController';
+	my $login = 'veritas';
+	my $userArticles = $modelObj->getA
+	print 'UserArticlesController';
 }
 
 1;
