26ba4187cbaf8bc744deb278e9551da6ee08a70f
Student 2 <user2@tc.geeksforless.net>
2017-09-01 17:18:54 +0300

    profEdit

--- application/controllers/ControllerProfileEdit.pm
+++ application/controllers/ControllerProfileEdit.pm
@@ -4,19 +4,29 @@ use strict;
 use warnings;
 
 use vars qw(@ISA);
+use vars qw(%in);
 use core::Controller;
+use core::Controller;
+use models::Users;
+use views::ViewProfileEdit;
+use Data::Dumper;
+use CGI;
 @ISA = qw(core::Controller);
 
+my $model = models::Users->new();
+my $var = new CGI->new();
 
 
 sub actionIndex
-{
-	my $model = models::Users->new();
-    my $getDataUserById = $model->getUserData('test');
-    my $editUser =  $model->actionEdit('test');
+{   
+	my $email = $var->param('email');
+	my $name = $var->param('name');
+	my $serName = $var->param('serName');
+    my $getDataUserById = $model->getUserData('Alex');
+    my $editUser =  $model->actionEdit('Alex',$email,$name,$serName);
 	my $view = views::ViewProfileEdit->new();
 	my $data = $view->getTemplate('profile'); 
-    my $obj =  $view->generateTemplate($data,$editUser);
+    my $obj =  $view->generateTemplate($data,$getDataUserById);
 	$view->viewTemplate($obj);
 
 }
