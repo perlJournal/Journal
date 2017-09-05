2c1e4a98d211e24e4bcea7126b47b42c3285a2b0
Raybeedrill <demezis1994@gmail.com>
2017-09-05 19:27:12 +0300

    some fixes

--- application/controllers/ControllerProfileEdit.pm
+++ application/controllers/ControllerProfileEdit.pm
@@ -41,9 +41,13 @@ sub actionEdit
     my $email = $var->param('email');
 	my $name = $var->param('name');
 	my $serName = $var->param('serName');
+    my %cookieData = $model->getCookieData;
+    my $getDataUserById = $model->getUserDataById(%cookieData->{'id'});
+    $getDataUserById = $getDataUserById->[0];
+my    $login = $getDataUserById->{'login'}  ; 
     $model->emptyString($email,$name,$serName);
-     my $editUser =  $model->actionEdit('Alex',$email,$name,$serName);
-	 my $getDataUserById = $model->getUserData('Alex');
+     my $editUser =  $model->actionEdit($login,$email,$name,$serName);
+	 my $getDataUserById = $model->getUserData($login);
 	 my $view = views::ViewProfileEdit->new();
 	 my $data = $view->getTemplate('profile');
 	 my $obj =  $view->generateTemplate($data,$getDataUserById );
