86271386b9085339a50caa21879a71f92f5770eb
Your Name <you@example.com>
2017-09-05 14:16:30 +0300

    log in main

--- application/controllers/ControllerMain.pm
+++ application/controllers/ControllerMain.pm
@@ -6,24 +6,27 @@ use warnings;
 use vars qw(@ISA);
 use core::Controller;
 use models::Articles;
+use models::Users;
 use views::ViewMain;
 use Data::Dumper;
 
 @ISA = qw(core::Controller);
 
-my $modelObj = models::Articles->new();
+my $articleObj = models::Articles->new();
+my $userObj = models::Users->new();
 
 sub actionIndex
 {
+	my $check = $userObj->checkUser();
 
-	my @allArticles = @{$modelObj->getArticleAll()};
+	my @allArticles = @{$articleObj->getArticleAll()};
 	my @sorted = sort {$b->{'id_article'} <=> $a->{'id_article'}} @allArticles;
 	my $list = \@sorted;
+
 	my $view = views::ViewMain->new();
 	my $template = $view->getTemplate('mainTemplate');
-	my $page = $view->generateTemplate($template, $list);
+	my $page = $view->generateTemplate($template, $list, $check);
 	$view->viewTemplate($page);
-
 }
 
 
